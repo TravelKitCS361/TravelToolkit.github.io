6c41b148ff8d087e84f0dab626287e1e93eb2dad
Aldo Velasquez <AF15189@LC02WL10WHTDD>
2018-11-16 20:53:05 -0500

    added medical information request handler

---
+++ js/HttpRequest.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function getHostpitalInformation(city) {
+    $.ajax({
+        type: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json',
+        cache: false,
+        dataType: 'json',
+        data: {
+            input: city || 'richmond',
+            types: 'establishment',
+            radius: 50,
+            key: config.apiKey
+        },
+        success: function (resp) {
+            console.log('resp: ', resp);
+        },
+        error: function (error) {
+            console.error('error: ', error);
+        }
+    });
+}
+
+const HttpRequest = {
+    getHostpitalInformation: getHostpitalInformation
+}
